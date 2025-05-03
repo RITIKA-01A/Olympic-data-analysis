69e44a96a772363dd3f9f70b386c39b349a91f1c
RITIKA_01A <riti.bt22cs63@opju.ac.in>
2025-05-03 07:53:41 +0530

    added datasets to the root directory

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
 
-df = pd.read_csv(r"dataset\athlete_events.csv")
-region_df = pd.read_csv(r"dataset\noc_regions.csv")
+df = pd.read_csv(r"athlete_events.csv")
+region_df = pd.read_csv(r"noc_regions.csv")
 
 df = preprocessor.preprocess(df , region_df)
 
